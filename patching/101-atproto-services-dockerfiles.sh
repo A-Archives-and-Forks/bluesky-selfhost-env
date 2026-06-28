72252427b1e09c18c86d2277d9782e652b58b94b
itaru2622 <itaru2622@gmail.com>
2026-06-28 16:53:10 +0900

    remake: patching/101-atproto-services-dockerfiles.sh to fix runtime error on services/bsync/Dockerfile

--- patching/101-atproto-services-dockerfiles.sh
+++ patching/101-atproto-services-dockerfiles.sh
@@ -10,5 +10,6 @@ echo "applying patch: under ${d_} for services/*/Dockerfile"
 pushd ${d_}
 
 sed -i '/# Move files into the image and install/a COPY ./[a-zA-Z]*.* ./' services/*/Dockerfile
+sed -i '\|COPY ./services/bsync ./services/bsync|i COPY ./packages/bsync ./packages/bsync' services/bsync/Dockerfile
 
 popd
